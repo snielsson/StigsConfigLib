c6e548218771bcdbb40be9f50924a18019a61686
Stig Schmidt Nielsson <stigbn@gmail.com>
2018-07-24 12:22:48 +0200

    fixed dotSettings files

--- StigsConfigLib/ConfigurationBuilderExtensions.cs
+++ StigsConfigLib/ConfigurationBuilderExtensions.cs
@@ -9,7 +9,8 @@ using StigsUtilsLib;
 
 namespace StigsConfigLib {
 	public static class ConfigurationBuilderExtensions {
-		private static bool TryAddFile(this IConfigurationBuilder @this, string path, bool optional = false, bool reloadonChange = false) {
+		private static bool TryAddFile(this IConfigurationBuilder @this, string path, bool optional = false,
+			bool reloadonChange = false) {
 			if (!File.Exists(path)) return false;
 			switch (Path.GetExtension(path)) {
 				case ".json":
@@ -24,7 +25,9 @@ namespace StigsConfigLib {
 				default: throw new ArgumentException($"Unsupported configuration provider '{path}'.", path);
 			}
 		}
-		public static IConfigurationBuilder Add(this IConfigurationBuilder @this, string path, bool optional = false, bool reloadonChange = false) {
+
+		public static IConfigurationBuilder Add(this IConfigurationBuilder @this, string path, bool optional = false,
+			bool reloadonChange = false) {
 			path = Path.GetFullPath(Path.ChangeExtension(path, ""));
 			var xml = @this.TryAddFile(path + ".xml", optional, reloadonChange);
 			var json = @this.TryAddFile(path + ".json", optional, reloadonChange);
@@ -33,23 +36,28 @@ namespace StigsConfigLib {
 
 			return @this;
 		}
-		public static T Load<T>(this IConfigurationBuilder @this, string env = null, string[] args = null, string name = null, string directory = "ConfigurationFiles", bool optional = true, bool reloadonChange = true) where T : IConfigurationService<T> {
+
+		public static T Load<T>(this IConfigurationBuilder @this, string env = null, string[] args = null,
+			string name = null, string directory = "ConfigurationFiles", bool optional = true,
+			bool reloadonChange = true) where T : IConfigurationService<T> {
 			env = env ?? Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 			args = args ?? Environment.GetCommandLineArgs();
 			name = name ?? typeof(T).Name;
 			@this.Add($"{directory}/{name}.Default.", optional, reloadonChange);
-			if (env != null) {
-				@this.Add($"{directory}/{name}.{env}.", optional, reloadonChange);
-			}
+			if (env != null) @this.Add($"{directory}/{name}.{env}.", optional, reloadonChange);
 			@this.AddEnvironmentVariables().AddCommandLine(args);
 			var configurationRoot = @this.Build();
 			var result = configurationRoot.Get<T>();
 			if (reloadonChange) {
-				foreach (var provider in configurationRoot.Providers.OfType<FileConfigurationProvider>().Where(x => x.Source.ReloadOnChange)) {
-					var path = Path.Combine((provider.Source.FileProvider as PhysicalFileProvider)?.Root??"", provider.Source.Path);
-					provider.GetReloadToken().RegisterChangeCallback(x => result.OnConfigurationChangedEvent(path), path);
+				foreach (var provider in configurationRoot.Providers.OfType<FileConfigurationProvider>()
+					.Where(x => x.Source.ReloadOnChange)) {
+					var path = Path.Combine((provider.Source.FileProvider as PhysicalFileProvider)?.Root ?? "",
+						provider.Source.Path);
+					provider.GetReloadToken()
+						.RegisterChangeCallback(x => result.OnConfigurationChangedEvent(path), path);
 				}
 			}
+
 			return result;
 		}
 	}
